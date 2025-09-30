53a513b12b6a2fda21374f592b33bcd144dcca6e
Jair Oliveira (Ceifeirocv) <jair.almeida.oliveira@gmail.com>
2025-09-30 12:11:18 -0100

    fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getInitials(name: string) {
+  const words = name.trim().split(/\s+/);
+  if (words.length === 0) return "";
+  if (words.length === 1) return words[0].charAt(0).toUpperCase();
+
+  const firstInitial = words[0].charAt(0).toUpperCase();
+  const lastInitial = words[words.length - 1].charAt(0).toUpperCase();
+  return firstInitial + lastInitial;
 }
